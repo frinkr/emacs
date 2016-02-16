5c3a55429fcb34e99ea099e82cffec00670a044d
Yuqing Jiang <frinkr@outlook.com>
2016-02-16 10:34:19 +0800

    add keyboard shortcut for maggit-status

--- emacs.el
+++ emacs.el
@@ -508,8 +508,11 @@
 ;;;;
 (when t
   (require 'magit)
+
   (setq magit-last-seen-setup-instructions "1.4.0")
   (setq magit-auto-revert-mode nil)
+  
+  (global-set-key (kbd "C-x g") 'magit-status)  
   )
 
 
