260360ad034a0dd2e85faa59e4cbe19378ee537d
Yuqing Jiang <frinkr@outlook.com>
2017-11-28 11:31:47 +0800

    quit magit buffers on committing

--- emacs.el
+++ emacs.el
@@ -913,10 +913,20 @@
   (global-set-key (kbd "C-x g") 'magit-status)
   (defalias 'magit 'magit-status)
 
-  (define-key magit-mode-map 
-    (kbd "q") 
-    (lambda() (interactive) (magit-mode-bury-buffer t)))
-  
+  (defun kill-magit-diff-buffer-in-current-repo (&rest _)
+    "Delete the magit-diff buffer related to the current repo"
+    (let ((magit-diff-buffer-in-current-repo
+           (magit-mode-get-buffer 'magit-diff-mode)))
+      (kill-buffer magit-diff-buffer-in-current-repo)))
+  ;;
+  ;; When 'C-c C-c' is pressed in the magit commit message buffer,
+  ;; delete the magit-diff buffer related to the current repo.
+  ;;
+  (add-hook 'git-commit-setup-hook
+            (lambda ()
+              (add-hook 'with-editor-post-finish-hook
+                        #'kill-magit-diff-buffer-in-current-repo
+                        nil t))) ; the t is important
   )
 
 
