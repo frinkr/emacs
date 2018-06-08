8c0054f259d214ff146a0486c10bd323cec5918f
Yuqing Jiang <frinkr@outlook.com>
2018-06-08 10:31:03 +0800

    SPC: fix typo

--- spacemacs-layers/efx/funcs.el
+++ spacemacs-layers/efx/funcs.el
@@ -327,8 +327,8 @@
                               '(("\\<Q[A-Z][A-Za-z]*" . 'qt-keywords-face)))
       )
     )
-  (add-hook 'c-mode-common-hook 'fx/c-c++-mode-hook)
-  (add-hook 'c++-mode-common-hook 'fx/c-c++-mode-hook)
+  (add-hook 'c-mode-common-hook 'efx/c-c++-mode-hook)
+  (add-hook 'c++-mode-common-hook 'efx/c-c++-mode-hook)
 
   (defadvice c-lineup-arglist (around my activate)
     "Improve indentation of continued C++11 lambda function opened as argument."
